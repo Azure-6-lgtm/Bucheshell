96c457b2041e91430dddeac119fe3265f3d3262f
Azure-6-lgtm <bucherte46@gmail.com>
2026-04-15 22:31:51 +0530

    Fixed version

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     buchelog::log_warn("This is a warning example");
     buchelog::log_error("This is an error example");
     println!("---------------------------------------");
-    println!("{}","BUCHESHELL RELEASE 1.5.0 ENVIORMENT".red());
+    println!("{}","BUCHESHELL RELEASE 2.0.0 ENVIORMENT".red());
     println!("{}","AS SHELL IS NOT FULLY COMPLETE YET,IT CAN BE BUGGY".red());
     println!("{}","RELEASES ARE KNOWN TO BE UNSTABLE".red());
     println!("{}","BE CAUTIOUS OF BUGS".red());
